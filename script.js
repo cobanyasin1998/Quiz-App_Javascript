e4fc42bc4c40043fb60cc5db2ed88f2697377c93
Yasin Çoban <yasincoban1998@outlook.com>
2022-08-21 20:24:24 +0300

    timer_added

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const ui = new UI();
 
 ui.btn_start.addEventListener("click", function () {
   ui.quiz_box.classList.add("active");
+  startTimer(10);
   soruGoster(quiz.soruGetir());
   soruSayisiniGoster(quiz.soruIndex + 1, quiz.sorular.length);
   ui.btn_next.classList.remove("show");
@@ -12,15 +13,31 @@ ui.btn_next.addEventListener("click", function () {
   if (quiz.sorular.length != quiz.soruIndex) {
     document.querySelector(".quiz_box").classList.add("active");
     quiz.soruIndex += 1;
+    clearInterval(counter);
+    startTimer(10);
+  
     soruGoster(quiz.soruGetir());
     soruSayisiniGoster(quiz.soruIndex + 1, quiz.sorular.length);
     ui.btn_next.classList.remove("show");
   } else {
-    alert("QUIZ Bitti");
+    clearInterval(counter);
+
+    ui.score_box.classList.add("active");
+    ui.quiz_box.classList.remove("active");
+    ui.skoruGoster(quiz.sorular.length, quiz.dogruCevapSayisi);
   }
 });
 
+ui.btn_finish.addEventListener("click", function () {
+  window.location.reload();
+});
 
+ui.btn_replay.addEventListener("click", function () {
+  quiz.soruIndex = 0;
+  quiz.dogruCevapSayisi = 0;
+  ui.btn_start.click();
+  ui.score_box.classList.remove("active");
+});
 
 function soruGoster(soru) {
   let question = `<span>${soru.soruMetni}</span>`;
@@ -62,4 +79,37 @@ function optionsSelected(option) {
 function soruSayisiniGoster(soruSirasi, toplam) {
   let tag = `<span class="badge bg-warning">${soruSirasi} / ${toplam}</span>`;
   document.querySelector(".quiz_box .question_index").innerHTML = tag;
+}
+
+UI.prototype.skoruGoster = function (toplamSoru, dogruCevap) {
+  let tag = `Toplam ${toplamSoru} sorudan ${dogruCevap} doğru cevap verdiniz`;
+  document.querySelector(".score_box .score_text").innerHTML = tag;
+};
+let counter;
+function startTimer(time) {
+  counter = setInterval(timer, 1000);
+
+  function timer() {
+    ui.time_second.textContent = time;
+    time--;
+
+    if (time < 0) {
+      clearInterval(counter);
+
+      ui.time_text.textContent = "Süre Bitti";
+
+      let cevap =  quiz.soruGetir().dogruCevap;
+      
+      for(let option of ui.option_list.children){
+        if(option.querySelector("span b").textContent == cevap){
+          option.classList.add("correct");
+          option.insertAdjacentHTML("beforeend",ui.correctIcon);
+        }
+
+        option.classList.add("disabled");
+      }
+
+      ui.btn_next.classList.add("show");
+    }
+  }
 }
--- ui.js
+++ ui.js
@@ -2,6 +2,13 @@ function UI() {
   (this.btn_start = document.querySelector(".btn_start")),
     (this.btn_next = document.querySelector(".next_btn")),
     (this.quiz_box = document.querySelector(".quiz_box")),
+
+    (this.btn_replay = document.querySelector(".btn_replay")),
+    (this.btn_finish = document.querySelector(".btn_finish")),
+
+    (this.time_second = document.querySelector(".time_text")),
+
+    (this.score_box = document.querySelector(".score_box")),
     (this.option_list = document.querySelector(".option_list")),
     (this.correctIcon =  `<div class="icon"><i class="fas fa-check"></i></div>`),
     (this.inCorrectIcon =  `<div class="icon"><i class="fas fa-times"></i></div>`);
